c900acbb2ba582b12dfb447c397fd6e473399e77
Anuj Soni <anujsoni08@gmail.com>
2020-05-14 09:37:29 +0530

    added redux-thunk for async code

---
+++ src/containers/Counter/Counter.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
+import * as actionCreators from "../../store/actions/actions";
+
+class Counter extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <CounterOutput value={this.props.ctr} />
+        <CounterControl
+          label="Increment"
+          clicked={this.props.onIncrementCounter}
+        />
+        <CounterControl
+          label="Decrement"
+          clicked={this.props.onDecrementCounter}
+        />
+        <CounterControl
+          label="Add 5"
+          clicked={this.props.onIncrementBy5Counter}
+        />
+        <CounterControl
+          label="Subtract 5"
+          clicked={this.props.onDecrementBy5Counter}
+        />
+        <hr />
+        <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
+        <ul>
+          {this.props.storedResults.map((strResult) => (
+            <li
+              key={strResult.id}
+              onClick={() => this.props.onDeleteResult(strResult.id)}
+            >
+              {strResult.value}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    ctr: state.ctr.counter,
+    storedResults: state.res.results,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrementCounter: () => dispatch(actionCreators.increment()),
+    onDecrementCounter: () => dispatch(actionCreators.decrement()),
+    onIncrementBy5Counter: () =>
+      dispatch(actionCreators.incrementBy5(5)),
+    onDecrementBy5Counter: () =>
+      dispatch(actionCreators.decrementBy5(5)),
+    onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
+    onDeleteResult: (id) =>
+      dispatch(actionCreators.deleteResult(id)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
